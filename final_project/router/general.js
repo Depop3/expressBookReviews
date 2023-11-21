010b717433e5714a9ae533ad26c6fbf51fe7dbdd
Your Name <you@example.com>
2023-11-21 11:51:30 -0500

    message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const axios = require('axios').default;
 
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
@@ -30,7 +30,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
     res.send(books[isbn])
  });
   
-
+// Get all books based on author
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
     const booksByAuthor = Object.values(books).filter(book => book.author === author);
@@ -72,4 +72,53 @@ public_users.get('/review/:isbn', function (req, res) {
     }
 });
 
+// Get all books available with axios / promises
+const getBooks = axios.get("https://manueljramir-5001.theia-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/");
+console.log(getBooks);
+getBooks.then(resp => {
+    let Books = resp.data;
+    console.log(JSON.stringify(Books,null,4))
+})
+.catch(err => {
+    console.log("Error getting all books", err.toString())
+
+});
+
+// Get book by ISBN with axios / promises
+const getISBN = axios.get("https://manueljramir-5001.theia-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/1");
+console.log(getISBN);
+getISBN.then(resp => {
+    let book = resp.data;
+    console.log((book))
+})
+.catch(err => {
+    console.log("Error getting book by ISBN",err.toString())
+ });
+
+// Get all books based on author with axios / promises
+const getAuthor = axios.get("https://manueljramir-5001.theia-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/Samuel Beckett")
+console.log(getAuthor);
+getAuthor.then(resp => {
+    let Author = resp.data;
+    console.log((Author))
+})
+.catch(err => {
+    console.log("Error getting book by author", err.toString())
+
+});
+
+// Get all books based on title with axios / promises
+const getTitle = axios.get("https://manueljramir-5001.theia-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/Fairy tales")
+  .then(resp => {
+    let book = resp.data;
+    return book;
+  })
+  .catch(err => {
+    throw err; 
+  });
+
+getTitle.then(
+  (book) => console.log(book),
+  (err) => console.log("Error finding title", err)
+);
 module.exports.general = public_users;
